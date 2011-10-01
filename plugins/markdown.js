8dfcd6c40111bb6886069e92383aa93ead0363b7
Matt Zukowski <matt@zukowski.ca>
2011-10-01 17:40:54 -0400

    markdown plugin will now process @returns descriptions

--- plugins/markdown.js
+++ plugins/markdown.js
@@ -6,26 +6,38 @@
 
 var mdParser = require('evilstreak/markdown');
 
+function stripDefaultParagraph(html) {
+    return html.replace( /^<p>/g, "" ) // remove initial <p>
+               .replace( /<\/p>$/g, "" ) // remove trailing </p>
+}
+
+function unescapeEntities(html) {
+    return html.replace( /&amp;/g, "&" ) // because markdown escapes these
+               .replace( /&lt;/g, "<" )
+               .replace( /&gt;/g, ">" );
+}
+
 /**
     Translate markdown syntax in a new doclet's description into HTML. Is run
     by JSDoc 3 whenever a "newDoclet" event fires.
  */
 exports.newDoclet = function(e) {
     if (e.doclet.description) {
-        e.doclet.description = mdParser.toHTML(e.doclet.description)
-            .replace( /&amp;/g, "&" ) // because markdown escapes these
-            .replace( /&lt;/g, "<" )
-            .replace( /&gt;/g, ">" );
-    }
+        html = mdParser.toHTML(e.doclet.description)
+        e.doclet.description = unescapeEntities(html)
+     }
     if (e.doclet.params) {
         for (i in e.doclet.params) {
-            e.doclet.params[i].description = mdParser.toHTML(e.doclet.params[i].description)
-              .replace( /^<p>/g, "" ) // remove initial <p>
-              .replace( /<\/p>$/g, "" ) // remove trailing </p>
-              .replace( /&amp;/g, "&" ) // because markdown escapes these
-              .replace( /&lt;/g, "<" )
-              .replace( /&gt;/g, ">" );
- 
+            html = mdParser.toHTML(e.doclet.params[i].description)
+            html = unescapeEntities(stripDefaultParagraph(html))
+            e.doclet.params[i].description = html
+        }
+    }
+    if (e.doclet.returns) {
+        for (i in e.doclet.returns) {
+            html = mdParser.toHTML(e.doclet.returns[i].description)
+            html = unescapeEntities(stripDefaultParagraph(html))
+            e.doclet.returns[i].description = html
         }
     }
 };
